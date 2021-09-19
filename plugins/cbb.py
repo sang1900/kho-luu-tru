1288106f4c3da9fc49dad7b8b07b70eab4f0a7f4
sang1900 <nguyenvansangkto@gmail.com>
2021-09-19 15:48:46 +0700

    Cập nhật toàn bộ code

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Creator : @trumlinknong\n○ Version  : <code>Premium</code>\n○ Channel : @nunhiquoc\n○ Group : @daolinknong\n✉️ Hợp tác liên hệ : @trumlinknong\n\n‼️ Nhận tạo BOT tương tự như này với sever riêng. Quan tâm liên hệ @trumlinknong ‼️</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("❌ Đóng", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
